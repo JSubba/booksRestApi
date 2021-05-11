6acb3c7cde701b8c056823aec9c83c39d9db5c40
JSubba <prolearn1000@protonmail.com>
2021-05-11 18:45:13 +0900

    routes

--- routes/books.js
+++ routes/books.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const author = require("../models/author");
 const { Book, validateBook } = require("../models/books");
 
+//create books
 router.post("/", async (req, res) => {
     const error = await validateBook(req.body);
 
@@ -23,6 +24,55 @@ router.post("/", async (req, res) => {
         .catch((error) => {
             res.status(500).send("Book not stored in db");
         });
+});
+
+//fetch all books
+router.get("/", async (req, res) => {
+    let books = await Book.find().sort({ createdAt: "desc" });
+    try {
+        res.send(books);
+    } catch (error) {
+        res.status(500).send("Cannot fetch books");
+    }
+});
+
+//fetch book by Id
+router.get("/:bookId", async (req, res) => {
+    let book = await Book.findById(req.params.bookId);
+    try {
+        // if (book) res.send(book);
+        // res.status(404).send("Book not found");
+        res.send(book);
+    } catch (error) {
+        res.status(500).send("Cannot fetch books");
+    }
+});
+
+//update book
+router.put("/:bookId", async (req, res) => {
+    const updatedBook = await Book.findByIdAndUpdate(
+        req.params.bookId,
+        {
+            name: req.body.bookName,
+            author: {
+                name: req.body.authorName,
+                age: req.body.authorAge,
+            },
+            genre: req.body.genre,
+        },
+        { new: true }
+    );
+
+    if (!updatedBook) res.status(404).send("Book not updated");
+    res.send(updatedBook);
+});
+
+//delete book
+router.delete("/:bookId", async (req, res) => {
+    const deleteBook = await Book.findByIdAndDelete(req.params.bookId);
+    if (!deleteBook) res.status(404).send(`Book not deleted`);
+
+    res.send(deleteBook);
 });
 
 module.exports = router;
